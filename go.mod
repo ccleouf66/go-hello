6d3adc549f2b059b87ec23a72037f9cbed897b33
Cyril Connan <cyril.connan@gmail.com>
2023-03-07 10:44:17 +0000

    add chi router and add log

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ccleouf66/go-hello
 
 go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
+)
--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	  fmt.Fprintf(w, "Hello, %s !", r.URL.Path[1:])
-  })
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "application/json"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	// Public Routes
+	r.Group(func(r chi.Router) {
+		r.Get("/", index)
+		r.Get("/healthz", healthz)
+	})
+
+	log.Fatal(http.ListenAndServe(":3080", r))
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	io.WriteString(w, `{"msg": go-hello}`)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	io.WriteString(w, `{"alive": true}`)
+}
